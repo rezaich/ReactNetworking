494934760302606f048e5dec109ae67bbdd0726c
rezaich <ichsanireza@gmail.com>
2021-12-28 16:26:09 +0700

    learn new component in react native

--- App.js
+++ App.js
@@ -1,83 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { StyleSheet, View, Text, Image, Modal, TextInput, Alert, PermissionsAndroid, Button, TouchableOpacity } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Dimensions, StatusBar } from 'react-native';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
-function useInput() {
-  const [date, setDate] = useState(new Date());
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
-  const [dateText, setDateText] = useState('empty');
+const FirstRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
 
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
-  const showDatepicker = () => {
-    showMode('date');
-  };
+const SecondRoute = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date
-    setShow(Platform.OS === 'ios');
-    setDate(currentDate);
+const initialLayout = { width: Dimensions.get('window').width };
 
-    let tempDate = new Date(currentDate);
-    let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
-    let fTime = 'Hours: ' + tempDate.getHours() + ' | Minutes: ' + tempDate.getMinutes();
-    setDateText(fDate + '\n' + fTime)
-    console.log(fDate)
-  }
-  return {
-    date,
-    showDatepicker,
-    show,
-    mode,
-    onChange,
-    dateText
-  }
-}
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
 
-
-export default app = () => {
-  const input = useInput(new Date())
-  const input2 = useInput(new Date())
+export default function TabViewExample() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
 
   return (
-    <View style={{ flex: 1, padding: 5 }}>
-      <View
-        style={{ padding: 10, borderRadius: 5 }}>
-
-        <Button
-          onPress={input.showDatepicker}
-          title={input.dateText}
-          color={'#931A25'} />
-        {
-          input.show && (
-            <DateTimePicker
-              testID="dateTimePicker1"
-              value={input.date}
-              mode={input.mode}
-              display="default"
-              onChange={input.onChange} />
-          )
-        }
-      </View>
-      <View
-        style={{ padding: 10, borderRadius: 5 }}>
-
-        <Button
-          onPress={input2.showDatepicker}
-          title={input2.dateText}
-          color={'#931A25'} />
-        {input2.show && (
-          <DateTimePicker
-            testID="dateTimePicker2"
-            value={input2.date}
-            mode={input2.mode}
-            display="default"
-            onChange={input2.onChange} />
-        )}
-      </View>
-    </View>
-  )
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+      style={styles.container}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight,
+  },
+  scene: {
+    flex: 1,
+  },
+});
--- calenderPicker.js
+++ calenderPicker.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { StyleSheet, View, Text, Image, Modal, TextInput, Alert, PermissionsAndroid, Button, TouchableOpacity } from 'react-native';
 
-
 function useInput() {
     const [date, setDate] = useState(new Date());
     const [mode, setMode] = useState('date');
     const [show, setShow] = useState(false);
+    const [dateText, setDateText] = useState('empty');
 
     const showMode = (currentMode) => {
         setShow(true);
@@ -20,48 +20,64 @@ function useInput() {
         const currentDate = selectedDate || date
         setShow(Platform.OS === 'ios');
         setDate(currentDate);
+
+        let tempDate = new Date(currentDate);
+        let fDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+        let fTime = 'Hours: ' + tempDate.getHours() + ' | Minutes: ' + tempDate.getMinutes();
+        setDateText(fDate + '\n' + fTime)
+        console.log(fDate)
     }
     return {
         date,
         showDatepicker,
         show,
         mode,
-        onChange
+        onChange,
+        dateText
     }
 }
+
 
 export default app = () => {
     const input = useInput(new Date())
     const input2 = useInput(new Date())
 
     return (
-        <View style={{ flex: 1, flexDirection: 'row', padding: 5 }}>
-            <Button
-                onPress={input.showDatepicker}
-                title={input.date.toLocaleDateString("DD/MM/YYYY")}
-                color={'#931A25'} />
-            {
-                input.show && (
+        <View style={{ flex: 1, padding: 5 }}>
+            <View
+                style={{ padding: 10, borderRadius: 5 }}>
+
+                <Button
+                    onPress={input.showDatepicker}
+                    title={input.dateText}
+                    color={'#931A25'} />
+                {
+                    input.show && (
+                        <DateTimePicker
+                            testID="dateTimePicker1"
+                            value={input.date}
+                            mode={input.mode}
+                            display="default"
+                            onChange={input.onChange} />
+                    )
+                }
+            </View>
+            <View
+                style={{ padding: 10, borderRadius: 5 }}>
+
+                <Button
+                    onPress={input2.showDatepicker}
+                    title={input2.dateText}
+                    color={'#931A25'} />
+                {input2.show && (
                     <DateTimePicker
-                        testID="dateTimePicker1"
-                        value={input.date}
-                        mode={input.mode}
+                        testID="dateTimePicker2"
+                        value={input2.date}
+                        mode={input2.mode}
                         display="default"
-                        onChange={input.onChange} />
-                )
-            }
-            <Button
-                onPress={input2.showDatepicker}
-                title={input2.date.toLocaleDateString()}
-                color={'#931A25'} />
-            {input2.show && (
-                <DateTimePicker
-                    testID="dateTimePicker2"
-                    value={input2.date}
-                    mode={input2.mode}
-                    display="default"
-                    onChange={input2.onChange} />
-            )}
+                        onChange={input2.onChange} />
+                )}
+            </View>
         </View>
     )
 }
